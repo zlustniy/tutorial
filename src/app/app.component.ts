beb83a59c6a154a67e1873af4a0d3ba251b944b7
Max Zakirov <maksim.zakirov@zyfra.com>
2023-06-12 14:09:00 +0300

    tmp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,94 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, TemplateRef, ViewChild} from '@angular/core';
+import {User} from './models/user';
+import {UserService} from './services/user.service';
 
 @Component({
-  selector: 'app-root',
+  selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  providers: [UserService]
 })
-export class AppComponent {
-  title = 'tutorial';
+export class AppComponent implements OnInit {
+  title = 'usersdd';
+  //типы шаблонов
+  @ViewChild('readOnlyTemplate', {static: false}) readOnlyTemplate: TemplateRef<any> | undefined;
+  @ViewChild('editTemplate', {static: false}) editTemplate: TemplateRef<any> | undefined;
+
+  editedUser: User | null = null;
+  users: Array<User>;
+  isNewRecord: boolean = false;
+  statusMessage: string = "";
+
+  constructor(private serv: UserService) {
+    this.users = new Array<User>();
+  }
+
+  ngOnInit() {
+    this.loadUsers();
+  }
+
+  //загрузка пользователей
+  private loadUsers() {
+    this.serv.getUsers().subscribe((data: Array<User>) => {
+      this.users = data;
+    });
+  }
+
+  // добавление пользователя
+  addUser() {
+    this.editedUser = new User("", "", 0);
+    this.users.push(this.editedUser);
+    this.isNewRecord = true;
+  }
+
+  // редактирование пользователя
+  editUser(user: User) {
+    this.editedUser = new User(user._id, user.name, user.age);
+  }
+
+  // загружаем один из двух шаблонов
+  loadTemplate(user: User) {
+    if (this.editedUser && this.editedUser._id === user._id) {
+      return this.editTemplate;
+    } else {
+      return this.readOnlyTemplate;
+    }
+  }
+
+  // сохраняем пользователя
+  saveUser() {
+    if (this.isNewRecord) {
+      // добавляем пользователя
+      this.serv.createUser(this.editedUser as User).subscribe(_ => {
+        this.statusMessage = 'Данные успешно добавлены',
+          this.loadUsers();
+      });
+      this.isNewRecord = false;
+      this.editedUser = null;
+    } else {
+      // изменяем пользователя
+      this.serv.updateUser(this.editedUser as User).subscribe(_ => {
+        this.statusMessage = 'Данные успешно обновлены',
+          this.loadUsers();
+      });
+      this.editedUser = null;
+    }
+  }
+
+  // отмена редактирования
+  cancel() {
+    // если отмена при добавлении, удаляем последнюю запись
+    if (this.isNewRecord) {
+      this.users.pop();
+      this.isNewRecord = false;
+    }
+    this.editedUser = null;
+  }
+
+  // удаление пользователя
+  deleteUser(user: User) {
+    this.serv.deleteUser(user._id).subscribe(_ => {
+      this.statusMessage = 'Данные успешно удалены',
+        this.loadUsers();
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,13 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
-import { AddTutorialComponent } from './components/add-tutorial/add-tutorial.component';
-import { TutorialDetailsComponent } from './components/tutorial-details/tutorial-details.component';
-import { TutorialsListComponent } from './components/tutorials-list/tutorials-list.component';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {AppComponent} from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    AddTutorialComponent,
-    TutorialDetailsComponent,
-    TutorialsListComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
+  imports: [BrowserModule, FormsModule, HttpClientModule],
+  declarations: [AppComponent],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
